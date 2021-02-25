76f3aca036f044bc7216a0443280f0a84309bf27
Emil Masiakowski <emil@empressia.pl>
2021-02-25 21:07:59 +0100

    feat: dispatch event on user granted access

--- src/Handler/SetTableUsersHandler.php
+++ src/Handler/SetTableUsersHandler.php
@@ -3,10 +3,14 @@
 namespace App\Handler;
 
 use App\Command\SetTableUsers;
+use App\Entity\UserInterface;
+use App\Event\UserEvent;
+use App\Events;
 use App\Repository\TableRepositoryInterface;
 use App\Repository\UserRepositoryInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class SetTableUsersHandler implements MessageHandlerInterface
 {
@@ -22,15 +26,25 @@ class SetTableUsersHandler implements MessageHandlerInterface
      * @var UserRepositoryInterface
      */
     private $userRepository;
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+    /**
+     * @var UserInterface[]
+     */
+    private $usersGrantedAccess = [];
 
     public function __construct(
         TableRepositoryInterface $tableRepository,
         EntityManagerInterface $entityManager,
-        UserRepositoryInterface $userRepository
+        UserRepositoryInterface $userRepository,
+        EventDispatcherInterface $eventDispatcher
     ) {
         $this->tableRepository = $tableRepository;
         $this->entityManager = $entityManager;
         $this->userRepository = $userRepository;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function __invoke(SetTableUsers $command): void
@@ -38,6 +52,8 @@ class SetTableUsersHandler implements MessageHandlerInterface
         if (null === $table = $this->tableRepository->get($command->id())) {
             throw new \InvalidArgumentException(\sprintf('Table %s does not exist', $command->id()));
         }
+
+        $previousUsers = $table->getUsers();
 
         $table->clearUsers();
 
@@ -47,8 +63,21 @@ class SetTableUsersHandler implements MessageHandlerInterface
             }
 
             $table->addUser($user);
+
+            if (!\in_array($user, $previousUsers, true)) {
+                $this->usersGrantedAccess[] = $user;
+            }
         }
 
         $this->entityManager->persist($table);
+
+        $this->dispatchEvents();
+    }
+
+    private function dispatchEvents(): void
+    {
+        foreach ($this->usersGrantedAccess as $user) {
+            $this->eventDispatcher->dispatch(new UserEvent($user), Events::USER_ACCESS_TO_TABLE_GRANTED);
+        }
     }
 }
